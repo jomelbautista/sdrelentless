8d201791e0352287b9d5a87028ee48e5518abb09
Jomel Bautista <jomelbautista@Jomels-MBP.attlocal.net>
2022-05-03 23:37:12 -0700

    updated users controller

--- flask_app/controllers/users.py
+++ flask_app/controllers/users.py
@@ -2,7 +2,7 @@ from flask import render_template, request, redirect, session, flash
 from flask_app import app
 from flask_app.models.user import User
 
-@app.route('/home')
+@app.route('/')
 def home():
     return render_template('home.html')
 
